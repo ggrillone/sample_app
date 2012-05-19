803a90fa634ad3c9ee3c4ae361154f873a1dcce4
Gregory Grillone <greg.grillone@gmail.com>
2012-05-19 13:14:19 -0400

    Add sass styles and change default root page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,72 +7,72 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       	# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      	visit '/static_pages/home'
+      	visit root_path
       	page.should have_selector('h1', :text => 'Sample App')
     end
 
   	it "should have the base title" do
-  		visit '/static_pages/home'
+  		visit root_path
   		page.should have_selector('title',
   									:text => "#{base_title}")
   	end
 
     it "should not have a custom page title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => ' | Home')
     end
   end
 
   describe "Help page" do
   	it "should have the h1 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('h1', :text => 'Help')
   	end
 
   	it "should have the base title" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('title',
   									:text => "#{base_title}")
   	end
 
     it "should not have a custom page title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should_not have_selector('title', :text => ' | Help')
     end
   end
 
   describe "About page" do
   	it "should have the h1 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text => 'About Us')
   	end
 
   	it "should have the base title" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('title',
   									:text => "#{base_title}")
   	end
 
     it "should not have a custom page title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should_not have_selector('title', :text => ' | About Us')
     end
   end
 
   describe "Contact page" do
   	it "should have the h1 'Contact'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should have_content("Contact")
   	end
 
   	it "should have the base title" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should have_selector('title',
   									:text => "#{base_title}")
   	end
 
     it "should not have a custom page title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should_not have_selector('title', :text => ' | Contact')
     end
   end
